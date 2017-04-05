dbe0a5e521050d9c69a4a80705a6c0cf0a6a6584
Billy Troy Wooton <gcowootont@gmail.com>
2017-04-04 15:12:13 -1000

    Made constructor for Tank class

--- src/Tank.java
+++ src/Tank.java
@@ -1,4 +1,26 @@
 
 public class Tank {
+	
+	private EZImage tankSprite;
+	private float hp;
+	private float speed;
+	private float x, y;
+	private float w, h;
+	private float weaponPower;
+	private boolean shielded;
+
+	
+	public Tank(String imageName, int _x, int _y) {
+		tankSprite = EZ.addImage(imageName, _x, _y);
+		hp = 100;
+		speed = 1;
+		x = _x;
+		y = _y;
+		w = tankSprite.getWidth();
+		h = tankSprite.getHeight();
+		weaponPower = 10;
+		shielded = false;
+	}
+	
 
 }
