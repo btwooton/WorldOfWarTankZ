15ffa0ae20ff1156414ed1f0233f3afa5124e6eb
kaleYard <gilbertproductions@gmail.com>
2017-04-04 14:09:34 -1000

    added ez and powerup

---
+++ src/PowerUp.java
@@ -0,0 +1,12 @@
+
+public class PowerUp {
+	private int x, y;
+	public boolean onSpeed;
+	public boolean onKit;
+	public boolean onShield;
+	public boolean onWeapon;
+	EZImage repairKit;
+	EZ speedUpgrade;
+	String Shield;
+	String weaponUpgrade;
+}
