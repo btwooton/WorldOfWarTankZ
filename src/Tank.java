9ca4d1be4a09ae80a238b09d1c772202a0de79d5
Billy Troy Wooton <gcowootont@gmail.com>
2017-04-04 15:40:10 -1000

    Implemented takeDamage() and moveAround() methods

--- src/Tank.java
+++ src/Tank.java
@@ -2,15 +2,16 @@
 public class Tank {
 	
 	private EZImage tankSprite;
-	private float hp;
-	private float speed;
-	private float x, y;
-	private float w, h;
-	private float weaponPower;
+	private int hp;
+	private double speed;
+	private double x, y;
+	private int w, h;
+	private int weaponPower;
+	private char directionUp, directionLeft, directionDown, directionRight;
 	private boolean shielded;
 
 	
-	public Tank(String imageName, int _x, int _y) {
+	public Tank(String imageName, int _x, int _y, char[] directions) {
 		tankSprite = EZ.addImage(imageName, _x, _y);
 		hp = 100;
 		speed = 1;
@@ -20,7 +21,29 @@ public class Tank {
 		h = tankSprite.getHeight();
 		weaponPower = 10;
 		shielded = false;
+		directionUp = directions[0];
+		directionLeft = directions[1];
+		directionDown = directions[2];
+		directionRight = directions[3];
 	}
 	
+	public void takeDamage(float amount) {
+		hp -= amount;
+	}
+	
+	public void moveAround(int worldHeight, int worldWidth) {
+		if (EZInteraction.isKeyDown(directionUp) && y > 0) {
+			tankSprite.translateTo(x, y -= speed);
+		}
+		else if (EZInteraction.isKeyDown(directionLeft) && x > 0) {
+			tankSprite.translateTo(x -= speed, y);
+		}
+		else if (EZInteraction.isKeyDown(directionDown) && y < worldHeight) {
+			tankSprite.translateTo(x, y += speed);
+		}
+		else if (EZInteraction.isKeyDown(directionRight) && x < worldWidth) {
+			tankSprite.translateTo(x += speed, y);
+		}
+	}
 
 }
