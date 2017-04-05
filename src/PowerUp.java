0b8f6dd58780807b2e5f4d0998fda48148d7db96
kaleYard <gilbertproductions@gmail.com>
2017-04-04 15:15:39 -1000

    powerUp enum constructer, started porjectiles class

--- src/PowerUp.java
+++ src/PowerUp.java
@@ -1,12 +1,14 @@
 
 public class PowerUp {
 	private int x, y;
-	public boolean onSpeed;
-	public boolean onKit;
-	public boolean onShield;
-	public boolean onWeapon;
-	EZImage repairKit;
-	EZ speedUpgrade;
-	String Shield;
-	String weaponUpgrade;
+	private Name name;
+	private static enum Name {
+		weaponUpgrade,
+		repairKit,
+		speedUp,
+		sheild,
+	}
+	
 }
+	
+
