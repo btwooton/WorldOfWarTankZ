9431ba0c256c6b4d9bfcdc15a54fad0573df848e
Jeffrey Bernaldez <Jeffrey@Jeffreys-MacBook-Air.local>
2017-04-17 12:25:23 -1000

    Health

--- src/Prototype3.java
+++ src/Prototype3.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Prototype3 {
 
-	static int x, y, HealthP1, width, height, Swidth, Sheight;
+	static int x, y, HealthP1, width, height, Swidth, Sheight, score, score2;
 	static int screenWidth = 1280;
 	static int screenHeight = 736;
 	static Projectiles[] projectiles = new Projectiles[100];
@@ -16,6 +16,8 @@ public class Prototype3 {
 	static PowerUp[] power = new PowerUp[50];
 	static Random rg = new Random();
 	static int counter = 0;
+	static EZText scoreDisplay, scoreDisplay2;
+	static String Points, Points2;
 
 	public static void main(String[] args) throws java.io.IOException {
 
@@ -31,6 +33,13 @@ public class Prototype3 {
 		// draw my character
 		Tank Player1 = new Tank("Tank.png", screenWidth *3 / 4, screenHeight / 2, 1, new char[] { 'w', 'a', 's', 'd' });
 		Tank Player2 = new Tank("Tank.png", screenWidth / 4, screenHeight / 2, 2, new char[] { 'i', 'j', 'k', 'l' });
+		
+		score = 100;
+		score2 = 100;
+		Points = "Player 1 HP: " + score;
+		Points2 = "Player 2 HP: " + score2;
+		scoreDisplay = EZ.addText(300, 15, Points, Color.BLACK, 25);
+		scoreDisplay2 = EZ.addText(screenWidth-300, 15, Points2, Color.BLACK, 25);
 
 		for (int i = 0; i < projectiles.length; i++) {
 			projectiles[i] = new Projectiles(-100, -100);
@@ -78,12 +87,21 @@ public class Prototype3 {
 					projectiles[i].ObstacleRicochet(xpos, ypos);
 
 					if (Player1.collideWithProjectiles(projectiles[i])) {
+						EZ.removeEZElement(scoreDisplay);
+						score -= 5;
+						Points = "Player 1 HP: " + score;
+						scoreDisplay = EZ.addText(300, 15, Points, Color.BLACK, 25);
 						projectiles[i].setOffScreen(screenHeight, screenWidth, true);
 						projectiles[i].translateTo(-100, -100);
 						Player1.takeDamage(projectiles[i].getFirePower());
+						
 					}
 
 					if (Player2.collideWithProjectiles(projectiles[i])) {
+						EZ.removeEZElement(scoreDisplay2);
+						score2 -= 5;
+						Points2 = "Player 2 HP: " + score2;
+						scoreDisplay2 = EZ.addText(screenWidth-300, 15, Points2, Color.BLACK, 25);
 						projectiles[i].setOffScreen(screenHeight, screenWidth, true);
 						projectiles[i].translateTo(-100, -100);
 						Player2.takeDamage(projectiles[i].getFirePower());
