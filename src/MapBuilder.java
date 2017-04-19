fa0ab0974f9346b40df87b9e6ba009978c567186
Jeffrey Bernaldez <Jeffrey@dhcp-168-105-28-229.wireless.manoa.hawaii.edu>
2017-04-19 09:19:27 -1000

    update

--- src/MapBuilder.java
+++ src/MapBuilder.java
@@ -13,40 +13,43 @@ public class MapBuilder {
 	static int bx, by;
 
 	public MapBuilder(String TextMap) throws java.io.IOException {
-		//set up scanner
+		// set up scanner
 		Scanner fileScanner = new Scanner(new FileReader(TextMap));
 
-		//scan map dimensions
+		// scan map dimensions
 		Swidth = fileScanner.nextInt();
 		Sheight = fileScanner.nextInt();
 		String inputText = fileScanner.nextLine();
+		// set up resolution
 		screenWidth = Swidth * 32;
 		screenHeight = Sheight * 32;
 
-		//initialize window
+		// initialize window
 		EZ.initialize(Swidth * 32, Sheight * 32); // initialize the window
 		// set to white background
 		EZ.setBackgroundColor(new Color(255, 255, 255));
 
-		//set x and y array lists
+		// set x and y array lists
 		xs = new ArrayList<Integer>();
 		ys = new ArrayList<Integer>();
-		
+
 		// for loop to build map
 		for (int line = 0; line < Sheight; line++) {
 
+			// read next line
 			inputText = fileScanner.nextLine();
 			System.out.println(inputText);
 
 			for (int i = 0; i < inputText.length(); i++) {
 
+				// read next character in line
 				char ch = inputText.charAt(i);
 
-				//object placement case
+				// object placement case
 				switch (ch) {
 				case 'D':
 					EZ.addImage("dirt.png", i * 32 + 16, line * 32 + 16);
-					//add posiion to arrays
+					// add position to arrays
 					xs.add(i * 32 + 16);
 					ys.add(line * 32 + 16);
 					break;
@@ -58,11 +61,13 @@ public class MapBuilder {
 			}
 		}
 	}
-	//array pulling methods
+
+	// array pulling methods
 	public ArrayList<Integer> getXList() {
-	       return xs;
+		return xs;
 	}
+
 	public ArrayList<Integer> getYList() {
-	       return ys;
+		return ys;
 	}
 }
