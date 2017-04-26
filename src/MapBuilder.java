a37f9be81ea4a3397d1e41666d64dd3143a2e099
Jeffrey Bernaldez <Jeffrey@Jeffreys-MacBook-Air.local>
2017-04-25 15:00:04 -1000

    random

--- src/MapBuilder.java
+++ src/MapBuilder.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.io.FileReader;
-import java.util.Scanner;
 import java.util.*;
 
 public class MapBuilder {
@@ -14,6 +13,7 @@ public class MapBuilder {
 	public MapBuilder(String TextMap) throws java.io.IOException {
 		// set up scanner
 		Scanner fileScanner = new Scanner(new FileReader(TextMap));
+		Random rg = new Random();
 
 		// scan map dimensions
 		Swidth = fileScanner.nextInt();
@@ -27,17 +27,27 @@ public class MapBuilder {
 		EZ.initialize(Swidth * 32, Sheight * 32); // initialize the window
 		// set to white background
 		EZ.setBackgroundColor(new Color(255, 255, 255));
-		//EZ.addImage("rockTexture.png", screenWidth / 2, screenHeight / 2);
+		
+		int level = rg.nextInt(2);
+		
 		while(true) {
-			
-			EZText message = EZ.addText(screenWidth / 2, screenHeight *3 / 4, "Press Space to Start", Color.black, 50);
+			EZImage start = EZ.addImage("Title.jpg", screenWidth / 2, screenHeight / 2);
+			EZImage title = EZ.addImage("WWTZLogo.png", screenWidth / 2, screenHeight / 4);
+			EZText message = EZ.addText(screenWidth / 2, screenHeight *3 / 4, "Press Space to Start", Color.white, 50);
 			if (EZInteraction.wasKeyReleased(KeyEvent.VK_SPACE)) {
+				EZ.removeEZElement(message);
+				EZ.removeEZElement(start);
 				EZ.removeEZElement(message);
 				break;
 			}
 		}
 		
-		EZ.addImage("blueTexture.png", screenWidth / 2, screenHeight / 2);
+		if (level==0) {
+			EZ.addImage("blueTexture.png", screenWidth / 2, screenHeight / 2);
+			}
+		if (level==1) {
+			EZ.addImage("rockTexture.png", screenWidth / 2, screenHeight / 2);
+			}
 
 		// set x and y array lists
 		xs = new ArrayList<Integer>();
@@ -58,16 +68,18 @@ public class MapBuilder {
 				// object placement case
 				switch (ch) {
 				case 'D':
-					EZImage brick = EZ.addImage("ground.png", i * 32 + 16, line * 32 + 16);
+					if (level==0) {
+						EZ.addImage("ground.png", i * 32 + 16, line * 32 + 16);
+					}
 					//EZ.addImage("tankTrap_32.png", i * 32 + 16, line * 32 + 16);
 
 					// add image
-					EZ.addImage("dirt.png", i * 32 + 16, line * 32 + 16);
+					if (level==1) {
+						EZ.addImage("dirt.png", i * 32 + 16, line * 32 + 16);
+					}
 					// add position to arrays
 					xs.add(i * 32 + 16);
 					ys.add(line * 32 + 16);
-					brick.getXCenter();
-					brick.getYCenter();
 					break;
 				default:
 					// Do nothing
